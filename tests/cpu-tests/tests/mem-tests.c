7ec42d5d81f90c635c9e445d485b657b32cb9cdf
ysyx_22050291-孙安东 <1044238861@qq.com>
2024-08-12 15:09:37 +0800

    icache

--- tests/cpu-tests/tests/mem-tests.c
+++ tests/cpu-tests/tests/mem-tests.c
@@ -1,14 +1,14 @@
 #include <stdint.h>
 void putch(char ch);
 
-volatile uint8_t *aa = (volatile uint8_t *)(uintptr_t)(0xc0000000);
+volatile uint8_t *aa = (volatile uint8_t *)(uintptr_t)(0xa0010000);
 
 int main() {
     uint32_t num = 0x83463931;
-    for (uint32_t i = 0; i < (uint32_t)0x1000; i++){
+    for (uint32_t i = 0; i < (uint32_t)0x100; i++){
         *(aa + i) = (uint8_t)(num + i);
     }
-    for (uint32_t j = 0; j < (uint32_t)0x1000; j++) {
+    for (uint32_t j = 0; j < (uint32_t)0x100; j++) {
         if (aa[j] != (uint8_t)(num + j)) {
             putch('0');
             putch('\n');
